13dcb1fc3b2abdc03b127445590dcbd9ab0f1ff1
BenXUser <rubenxaviernunes@gmail.com>
2024-12-14 13:14:33 +0100

    Added Kernel.isRunning Adapted Kernel.stop Adapted Kernel.start Adapted Kernel.addTask Added Kernel.taskLinkedList Added Kernel.Kernel

--- src/Kernel.java
+++ src/Kernel.java
@@ -1,28 +1,65 @@
+import lib.exceptions.NotElementComparableException;
 import lib.lists.LinkedList;
 
 public class Kernel {
 
-
+	LinkedList<Task> taskLinkedList;
 	boolean isRunning;
 
 	public Kernel() {
 
+		this.taskLinkedList = new LinkedList<Task>();
+		this.isRunning = true;
 	}
 
+
+	public boolean isRunning() {
+		return this.isRunning;
+	}
+
+
 	public void start() {
+
+		if (this.isRunning) {
+			System.out.println("KERNEL ALREADY STARTED");
+			return;
+		}
+
 		System.out.println("KERNEL STARTING...");
+
 		isRunning = true;
 		System.out.println("KERNEL STARTED");
 	}
 
 	public void stop() {
+
+		if (!this.isRunning) {
+			System.out.println("KERNEL ALREADY STOPPED");
+		}
+
 		System.out.println("KERNEL STOPPING...");
+		// TODO: CPU, MEMORY, DEVICES, SERVER
 		isRunning = false;
 		System.out.println("KERNEL STOPPED");
 	}
 
-	public void addTask() {
-		// Linked List for tasks?
+	public void addTask(Task task) throws NotElementComparableException {
+
+		if (!this.isRunning) {
+			System.out.println("KERNEL DID NOT STARTED YET");
+			return;
+		}
+
+		if (task == null) {
+			System.out.println("TASK CANNOT BE NULL");
+			return;
+		}
+
+
+		if (this.isRunning) {
+			this.taskLinkedList.add(task);
+			System.out.println("New task " + task.getName() + " was added.");
+		}
 	}
 
 }
